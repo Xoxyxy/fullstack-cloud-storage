2cfd378b8f755064c857132cc65d1b1ab8e2aba8
xoxu <dvinakovich@gmail.com>
2023-10-10 23:59:57 +0300

    Added swagger. Added typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        synchronize: true,
+      }),
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [ConfigModule, AppService],
 })
 export class AppModule {}
