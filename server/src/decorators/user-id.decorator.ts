5263344d68dee6c391f293cffe538307ff1da371
Daniil <dvinakovich@gmail.com>
2023-11-27 22:04:15 +0300

    Added docker-compose for db

--- server/src/decorators/user-id.decorator.ts
+++ server/src/decorators/user-id.decorator.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const UserId = createParamDecorator(
   (_: unknown, ctx: ExecutionContext): number | null => {
     const request = ctx.switchToHttp().getRequest();
-    console.log(request.user);
     return request.user?.id ? Number(request.user.id) : null;
   },
 );
